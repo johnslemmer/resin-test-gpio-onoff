1ef8a9b6f99a36f7e3cc3a8cc25011179174418e
John Slemmer <john.slemmer@gmail.com>
2018-09-08 17:27:02 -0700

    use onoff instead

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-var  ledToggle, pressCount;
-//provision the gpio pins 22 for the led output and 17 for the button input
-var led= require("pi-pins").connect(22),
-    button = require("pi-pins").connect(17);
+const Gpio = require('onoff').Gpio;
 
-//set the pin mode,  setting pin 22 as an output and 17 as an input
-button.mode('in');
-led.mode('out');
+const led = new Gpio(17, 'out');
+const button = new Gpio(4, 'in', 'both');
 
-//set the initial value of the LED to be off.
-ledToggle = false;
-pressCount= 0;
-led.value(true);
-
-//look for a button press event and switch on the LED for 2 seconds when this happens.
-button.on('rise', function () {
-    console.log("button pressed: "+ (++pressCount) +" time(s)");
-    ledToggle = !ledToggle;
-    led.value(ledToggle);
-});
+button.watch((err, value) => led.writeSync(value));
